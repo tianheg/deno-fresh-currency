ba8079a288d8d527713204fae8b2f4c6c32626b5
tianheg <i@tianheg.co>
2023-12-03 23:07:36 +0800

    initial commit

---
+++ fresh.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from "$fresh/server.ts";
+import twindPlugin from "$fresh/plugins/twind.ts";
+import twindConfig from "./twind.config.ts";
+
+export default defineConfig({ plugins: [twindPlugin(twindConfig)], port: 3000 });
